d2a25088ef443cbc5c9a85a8feaeaa47be7e358b
Qingping Hou <dave2008713@gmail.com>
2025-01-25 21:43:50 -0800

    add nvim-tree

--- nvim/init.lua
+++ nvim/init.lua
@@ -241,6 +241,21 @@ lazy_plugin_specs = {
   {"rhysd/vim-grammarous", ft = "markdown"},
 
   {
+    "nvim-tree/nvim-tree.lua",
+    event = "VeryLazy",
+    config = function()
+      -- disable netrw at the very start of your init.lua
+      vim.g.loaded_netrw = 1
+      vim.g.loaded_netrwPlugin = 1
+
+      -- optionally enable 24-bit colour
+      vim.opt.termguicolors = true
+
+      -- empty setup using defaults
+      require("nvim-tree").setup()
+    end,
+  },
+  {
     "ibhagwan/fzf-lua",
     event = "VeryLazy",
     dependencies = {
